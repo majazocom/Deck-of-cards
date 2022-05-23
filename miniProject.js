b0c4c91aeef023fb52951da0e7c049d733c5064d
Maja Gedda <maja.gedda@zocom.se>
2022-05-23 16:10:36 +0200

    added mini project and new files

---
+++ miniProject.js
@@ -0,0 +1,9 @@
+// Skämtmaskinen
+// I denna övning ska du bygga en mindre webbApp där du via en knapp slumpar 
+//fram ett skämt och skriver ut det på skärmen.
+// Du kommer behöva använda events, skapa HTML, arrayer, strings och Math.random();
+
+// Förslag på skämt hittar du här:
+// https://icanhazdadjoke.com/
+
+// Din webbapp ska vara stiligt CSS:ad.
