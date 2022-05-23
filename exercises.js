a2864311acc5d99e5bd651e3f789cfd5415d06dd
Maja Gedda <maja.gedda@zocom.se>
2022-05-23 14:17:50 +0200

    added exercises

---
+++ exercises.js
@@ -0,0 +1,34 @@
+//Vilka datatyper finns det i JS?
+let dataTypes = [];
+
+//Gör en array med 5 frukter i.
+
+
+//Lägg till en frukt i början och en frukt i slutet på arrayen.
+
+
+//I ovanstående fruktarray, plocka bort första och sista frukten.
+
+//I följande array, sätt in två nya frukter på index 2.
+let fruits1 = ['apple', 'orange', 'pear', 'kiwi']
+
+//Klona följande array och ändra första frukten till pineapple.
+let fruits2 = ['apple', 'orange', 'pear', 'kiwi']
+
+//Sortera följande array i fallande ordning.
+let num = [1, 5, 78, 7, 122, 3, 4, 65, 40, 2, 8]
+
+// Lägg ihop följande arrayer.
+let a1 = [1, 2, 3];
+let b1 = [4, 5, 6];
+
+//Mixa följande arrayer där varannan är från array a och varannan från array b.
+let a2 = ['My', 'has', 'many', 'open'];
+let b2 = ['brain', 'to', 'tabs', '!'];
+
+//Merga in array a i array b på index 2.
+let a3 = [1, 2, 7, 8, 9, 10];
+let b3 = [3, 4, 5, 6];
+
+//Gör alla namn i array names till versaler.
+let names = ['sixten', 'Eva', 'Ali', 'Kim', 'Greger', 'Alicia'];
